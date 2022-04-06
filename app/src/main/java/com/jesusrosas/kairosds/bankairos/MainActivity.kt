a1983b2832a99d5b761d76b1c4624f3441514212
jesus_rosas_kairosds_mx <jesus.rosas@kairosds.com>
2022-04-05 22:26:22 -0500

    Another trying

--- app/src/main/java/com/jesusrosas/kairosds/bankairos/MainActivity.kt
+++ app/src/main/java/com/jesusrosas/kairosds/bankairos/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jesusrosas.kairosds.bankairos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jesusrosas.kairosds.bankairos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
     }
 }
