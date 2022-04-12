5b01213a7fcd89e4fca92c99455da36782ea3fd7
jesus_rosas_kairosds_mx <jesus.rosas@kairosds.com>
2022-04-11 22:16:07 -0500

    Setting drawer menu

--- app/src/main/java/com/jesusrosas/kairosds/bankairos/ui/account/AccountFragment.kt
+++ app/src/main/java/com/jesusrosas/kairosds/bankairos/ui/account/AccountFragment.kt
@@ -1,16 +1,12 @@
 package com.jesusrosas.kairosds.bankairos.ui.account
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.jesusrosas.kairosds.bankairos.R
 import com.jesusrosas.kairosds.bankairos.databinding.AccountFragmentBinding
-import com.jesusrosas.kairosds.bankairos.databinding.OnBoardingFragmentBinding
-import com.jesusrosas.kairosds.bankairos.ui.onboarding.OnBoardingViewModel
 
 class AccountFragment : Fragment() {
 
@@ -27,7 +23,6 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
     }
 
