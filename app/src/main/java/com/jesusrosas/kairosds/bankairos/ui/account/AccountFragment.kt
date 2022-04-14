882540d36ffcf488cdcab3c4a3471992927b7087
jesus_rosas_kairosds_mx <jesus.rosas@kairosds.com>
2022-04-13 21:12:51 -0500

    Implementing drawer navigation

--- app/src/main/java/com/jesusrosas/kairosds/bankairos/ui/account/AccountFragment.kt
+++ app/src/main/java/com/jesusrosas/kairosds/bankairos/ui/account/AccountFragment.kt
@@ -1,24 +1,27 @@
 package com.jesusrosas.kairosds.bankairos.ui.account
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.jesusrosas.kairosds.bankairos.R
-import com.jesusrosas.kairosds.bankairos.data.model.TokenProvider
 import com.jesusrosas.kairosds.bankairos.databinding.AccountFragmentBinding
+import com.jesusrosas.kairosds.bankairos.toast
 
-class AccountFragment : Fragment() {
+class AccountFragment : Fragment(), NavigationView.OnNavigationItemSelectedListener {
 
     private val viewModel: AccountViewModel by viewModels()
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +35,8 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        drawerLayout = view.findViewById(R.id.drawer_layout)
+
         val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
 
         toolbar.setupWithNavController(
@@ -42,9 +47,21 @@ class AccountFragment : Fragment() {
             )
         )
 
-        Log.i("Debug", "${TokenProvider.token.token}")
+        val navigationView: NavigationView = view.findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
         viewModel.init()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.accounts -> activity?.toast("Accounts")
+            R.id.cards -> activity?.toast("Get Card")
+            R.id.loginFragment -> activity?.toast("Logout")
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
